ab60b15d0ae981c0c27fdb435605ed1bd9d77356
sntgmsml <53926500+sntgmsml@users.noreply.github.com>
2019-09-05 15:00:30 -0500

    Add files via upload

---
+++ PHP-login-master/conn.php
@@ -0,0 +1,7 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "";		     // database password
+$dbname	= "phplogin";    // database name
+?>
