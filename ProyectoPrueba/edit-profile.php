85d4a6d096500bd4a6284d4bb40cda9cb6c4d38d
SergioCuervoL <36489747+SergioCuervoL@users.noreply.github.com>
2019-09-05 14:46:05 -0500

    Add files via upload

--- ProyectoPrueba/edit-profile.php
+++ ProyectoPrueba/edit-profile.php
@@ -5,9 +5,9 @@ session_start();
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Editar Documento</title>
+    <title>Edit profile page</title>
 	
-
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -21,33 +21,32 @@ session_start();
     }
     else {
         echo "<div class='alert alert-danger mt-4' role='alert'>
-        <h4>Necesita iniciar sesión para acceder.</h4>
-        <p><a href='login.html'>Inicie sesión</a></p></div>";
+        <h4>You need to login to access this page.</h4>
+        <p><a href='login.html'>Login Here!</a></p></div>";
         exit;
     }
+    // checking the time now when check-login.php page starts
     $now = time();           
     if ($now > $_SESSION['expire']) {
         session_destroy();
         echo "<div class='alert alert-danger mt-4' role='alert'>
         <h4>Your session has expire!</h4>
-        <p><a href='login.html'>Inicie sesipon</a></p></div>";
+        <p><a href='login.html'>Login Here</a></p></div>";
         exit;
         }
     ?>
 
     <div class="container">
         <p>Welcome: <?php echo $_SESSION['name']; ?></p>
-        <h3>Edite su documento</h3>
-        <ul>
-            <li>Nombre</li>
-            <li>Descripción</li>
-            <li>Tamaño</li>
-            
-        </ul>
-        <p><a href="logout.php">Cerrar sesión</a></p>
+        <form action="documentUp.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="archivo" id="archivo"></input>
+            <input type="submit" value="subirArchivo">
+        </form>
+        <p><a href="logout.php">Logout</a></p>
     </div>
 
-	
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
--- ProyectoPrueba/phplogin.sql
+++ ProyectoPrueba/phplogin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-09-2019 a las 13:36:15
+-- Tiempo de generación: 05-09-2019 a las 20:36:15
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.4
 
