de1edc0f6e7f14de3cab3357cb79cdda604652d2
sntgmsml <53926500+sntgmsml@users.noreply.github.com>
2019-09-05 14:24:46 -0500

    Update check-login.php

--- ProyectoPrueba/check-login.php
+++ ProyectoPrueba/check-login.php
@@ -6,7 +6,7 @@ session_start();
 <html lang="en">
 	<head>
 		<title>Bienvenido a su cuenta</title>
-		<!-- Required meta tags -->
+		
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -17,35 +17,30 @@ session_start();
 		<div class="container">
 
 			<?php
-			// Connection info. file
+			
 			include 'conn.php';
 
-			// Connection variables
+			
 			$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-			// Check connection
+			
 			if (!$conn) {
 				die("Connection failed: " . mysqli_connect_error());
 			}
 
-			// data sent from form login.html
+			
 			$email = $_POST['email'];
 			$password = $_POST['password'];
 
-			// Query sent to database
+			
 			$result = mysqli_query($conn, "SELECT Email, Password, Name FROM users WHERE Email = '$email'");
 
-			// Variable $row hold the result of the query
 			$row = mysqli_fetch_assoc($result);
 
-			// Variable $hash hold the password hash on database
+			
 			$hash = $row['Password'];
 
-			/*
-			password_Verify() function verify if the password entered by the user
-			match the password hash on the database. If everything is OK the session
-			is created for one minute. Change 1 on $_SESSION[start] to 5 for a 5 minutes session.
-			*/
+			
 			if (password_verify($_POST['password'], $hash)) {
 
 				$_SESSION['loggedin'] = true;
@@ -63,8 +58,7 @@ session_start();
 			}
 			?>
 		</div>
-		<!-- Optional JavaScript -->
-		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
