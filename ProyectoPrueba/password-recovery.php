c17542e9019588efcb644d440dcc4a52bddb59a3
sntgmsml <53926500+sntgmsml@users.noreply.github.com>
2019-09-05 14:26:58 -0500

    Update password-recovery.php

--- ProyectoPrueba/password-recovery.php
+++ ProyectoPrueba/password-recovery.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
     	<title>Password Recovery</title>
-    	<!-- Required meta tags -->
+   
     	<meta charset="utf-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -20,7 +20,7 @@
 			$email = $_POST['email'];
 			$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-			// Check connection
+			
 			if (!$conn) {
 				die("Connection failed: " . mysqli_connect_error());
 			}
@@ -53,8 +53,7 @@
 		</div>
 	</div>
 </div>
-<!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
