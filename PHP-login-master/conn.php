9f83967a7862972817c060b1f7d7299d56c5dab1
sntgmsml <53926500+sntgmsml@users.noreply.github.com>
2019-09-05 15:12:03 -0500

    Delete conn.php

--- PHP-login-master/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Connection variables
-$dbhost	= "localhost";	   // localhost or IP
-$dbuser	= "root";		  // database username
-$dbpass	= "";		     // database password
-$dbname	= "phplogin";    // database name
-?>
